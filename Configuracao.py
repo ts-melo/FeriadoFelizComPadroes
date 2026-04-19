c25fa5d4d5ca9a65e8ed9679628ecada1ae67b65
tsmelo <ts.melo@unifesp.br>
2026-04-19 17:00:29 -0300

    singleton

---
+++ Configuracao.py
@@ -0,0 +1,9 @@
+class ConfiguracaoSistema:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(ConfiguracaoSistema, cls).__new__cls
+            cls._instance.api_key = "TOKEN"
+            cls._instance.ambiente = "PRODUCAO"
+        return cls._instance
